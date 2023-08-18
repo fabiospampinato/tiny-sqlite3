a3d4f166530f45b12b4e94d9f8f8a96cc8f9e276
Fabio Spampinato <spampinabio@gmail.com>
2023-08-18 19:11:12 +0100

    Ensuring the "Options" type is exported

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@
 
 import Database from './objects/database';
 import {raw, sql}  from './utils/database';
+import type {Options} from './types';
 
 /* EXPORT */
 
 export default Database;
 export {raw, sql};
+export type {Options};
